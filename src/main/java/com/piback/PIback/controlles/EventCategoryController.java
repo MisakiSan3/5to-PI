33689ec60ceab6af6999465c349e69ab4ec73a8a
Jair-Delgado <jar.delgado@yavirac.edu.ec>
2023-11-13 22:18:24 -0500

    Events tables

---
+++ src/main/java/com/piback/PIback/controlles/EventCategoryController.java
@@ -0,0 +1,18 @@
+/*import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.piback.PIback.models.User;
+
+public class EventCategoryController {
+    @Autowired
+    private EventCategoryService eventCategoryService;
+
+    @PostMapping("/save")
+    public User saveProveedores(@RequestBody User entity) {
+        
+        
+        return eventCategoryService.saveEventCategory(entity);
+    }
+    
+}*/
---
+++ src/main/java/com/piback/PIback/models/EventCategory.java
@@ -0,0 +1,22 @@
+package com.piback.PIback.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "event_category")
+public class EventCategory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "name_c", length = 50)
+    private String name_c;
+    
+}
