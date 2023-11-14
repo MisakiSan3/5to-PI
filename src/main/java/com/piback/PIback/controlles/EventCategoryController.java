9a135f95850dd8b0464f8ddc3c28b1bc27129f26
Jair-Delgado <jar.delgado@yavirac.edu.ec>
2023-11-13 23:18:14 -0500

    EventCategory (Service, controller and repository)

--- src/main/java/com/piback/PIback/controlles/EventCategoryController.java
+++ src/main/java/com/piback/PIback/controlles/EventCategoryController.java
@@ -1,18 +1,53 @@
-/*import org.springframework.beans.factory.annotation.Autowired;
+package com.piback.PIback.controlles;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import com.piback.PIback.models.User;
+import com.piback.PIback.models.EventCategory;
+import com.piback.PIback.services.EventCategoryService;
 
+@RestController
+@RequestMapping("api/events")
+@CrossOrigin({"*"})
 public class EventCategoryController {
     @Autowired
     private EventCategoryService eventCategoryService;
 
-    @PostMapping("/save")
-    public User saveProveedores(@RequestBody User entity) {
-        
+     @PostMapping("/save")
+    public EventCategory saveeventCategory(@RequestBody EventCategory entity) {
         
         return eventCategoryService.saveEventCategory(entity);
     }
-    
-}*/
+
+     @GetMapping("/{id}")
+    public EventCategory findeventCategory(@PathVariable long id)
+    {
+        return eventCategoryService.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    public EventCategory update(@RequestBody EventCategory entity)
+    {
+        return eventCategoryService.saveEventCategory(entity);
+    }
+
+     @DeleteMapping("/{id}")
+    public void deleteByID(@PathVariable long id)
+    {
+        eventCategoryService.deleteByID(id);
+    }
+
+    @GetMapping("/")
+    public Iterable<EventCategory> findAll()
+    {
+        return eventCategoryService.findAll();
+    }
+}
---
+++ src/main/java/com/piback/PIback/services/EventCategoryService.java
@@ -0,0 +1,31 @@
+package com.piback.PIback.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.piback.PIback.models.EventCategory;
+import com.piback.PIback.repositories.EventCategoryRepository;
+
+public class EventCategoryService {
+     @Autowired
+    private EventCategoryRepository eventCategoryRepository;
+
+     public EventCategory saveEventCategory(EventCategory EventCategory){
+
+        return eventCategoryRepository.save(EventCategory);
+    }
+
+    public EventCategory findById(long id)
+    {
+        return eventCategoryRepository.findById(id).orElse(null);
+    }
+    
+    public void deleteByID (long id)
+    {
+        eventCategoryRepository.deleteById(id);
+    }
+
+    public Iterable<EventCategory> findAll()
+    {
+        return eventCategoryRepository.findAll();
+    }
+}
